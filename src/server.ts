235f562f3c797a0ec3a3fd092d8b90247029a07b
JoaoVSouto <joaovitorss99@hotmail.com>
2020-11-03 15:23:31 -0300

    Adds VS Code debugger configuration

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 
+import routes from './routes';
+
 const app = express();
 
-app.get('/', (req, res) => res.json({ hello: 'gostack' }));
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333, () => console.log('Server listening on port 3333...'));
