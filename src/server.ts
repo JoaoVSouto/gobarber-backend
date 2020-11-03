b6925e4842885bf86dc6dfc133efb57c0b60e883
JoaoVSouto <joaovitorss99@hotmail.com>
2020-11-03 15:06:03 -0300

    Configures ESLint

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ hello: 'gostack' });
-});
+app.get('/', (req, res) => res.json({ hello: 'gostack' }));
 
 app.listen(3333, () => console.log('Server listening on port 3333...'));
