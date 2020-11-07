dad7039d349adb583335407d13cba52060405265
JoaoVSouto <joaovitorss99@hotmail.com>
2020-11-06 22:46:29 -0300

    Creates authentication checker middleware

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import authConfig from '../config/auth';
+
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+export default function ensureAuthenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    throw Error('Token is missing.');
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  const { secret } = authConfig.jwt;
+
+  try {
+    const { sub } = verify(token, secret) as TokenPayload;
+
+    req.user = { id: sub };
+
+    return next();
+  } catch {
+    throw Error('Invalid token.');
+  }
+}
