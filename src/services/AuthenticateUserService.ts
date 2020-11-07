adfc872fc324d719c73e417a150ee240876ef7b5
JoaoVSouto <joaovitorss99@hotmail.com>
2020-11-06 22:19:15 -0300

    Generates JWT token when session is created

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: Optional<User, 'password'>;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -28,7 +30,12 @@ class AuthenticateUserService {
       throw Error('Incorrect email/password combination.');
     }
 
-    return { user };
+    const token = sign({}, '8538607221f2e42284acf599214cfa34', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
   }
 }
 
