1ae34295a2c32b448b3aa66ac438de15c2a4f64f
JoaoVSouto <joaovitorss99@hotmail.com>
2020-11-15 18:22:13 -0300

    Adds cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import 'express-async-errors';
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 
 import './database';
 
@@ -12,6 +13,7 @@ import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 
