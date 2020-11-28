d6baa64109acd37bde9354c7fcc874c086148eb9
JoaoVSouto <joaovitorss99@hotmail.com>
2020-11-28 13:33:21 -0300

    Creates service for resetting user password

--- src/modules/users/services/ResetPasswordService.ts
+++ src/modules/users/services/ResetPasswordService.ts
@@ -1,6 +1,6 @@
 import { injectable, inject } from 'tsyringe';
 
-// import AppError from '@shared/errors/AppError';
+import AppError from '@shared/errors/AppError';
 
 // import User from '@modules/users/infra/typeorm/entities/User';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
@@ -21,7 +21,23 @@ class ResetPasswordService {
     private userTokensRepository: IUserTokensRepository,
   ) {}
 
-  public async execute({ token, password }: IRequest): Promise<void> {}
+  public async execute({ token, password }: IRequest): Promise<void> {
+    const userToken = await this.userTokensRepository.findByToken(token);
+
+    if (!userToken) {
+      throw new AppError('User token does not exist.');
+    }
+
+    const user = await this.usersRepository.findById(userToken?.user_id);
+
+    if (!user) {
+      throw new AppError('User does not exist.');
+    }
+
+    user.password = password;
+
+    await this.usersRepository.save(user);
+  }
 }
 
 export default ResetPasswordService;
