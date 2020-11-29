3abdf28ed4478a5edacded1ca71dffb0d2f050df
JoaoVSouto <joaovitorss99@hotmail.com>
2020-11-29 11:34:56 -0300

    Implements checks to see if the token is expired

--- src/modules/users/services/ResetPasswordService.ts
+++ src/modules/users/services/ResetPasswordService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import { differenceInHours } from 'date-fns';
 
 import AppError from '@shared/errors/AppError';
 
@@ -36,6 +37,12 @@ class ResetPasswordService {
 
     if (!user) {
       throw new AppError('User does not exist.');
+    }
+
+    const tokenCreatedAt = userToken.created_at;
+
+    if (differenceInHours(Date.now(), tokenCreatedAt) > 2) {
+      throw new AppError('Token expired');
     }
 
     user.password = await this.hashProvider.generateHash(password);
