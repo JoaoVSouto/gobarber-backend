429402ef60439e99cb2523164efa372be1572935
JoaoVSouto <joaovitorss99@hotmail.com>
2020-12-06 18:37:27 -0300

    Sets to false appointments on past date

--- src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -16,13 +16,17 @@ describe('ListProviderDayAvailability', () => {
   it('should be able to list the day availability from provider', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: '123',
-      date: new Date(2020, 11, 5, 8, 0, 0),
+      date: new Date(2020, 11, 5, 14, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: '123',
-      date: new Date(2020, 11, 5, 10, 0, 0),
+      date: new Date(2020, 11, 5, 15, 0, 0),
     });
+
+    jest
+      .spyOn(Date, 'now')
+      .mockImplementationOnce(() => new Date(2020, 11, 5, 11).getTime());
 
     const availability = await listProviderDayAvailability.execute({
       provider_id: '123',
@@ -34,9 +38,12 @@ describe('ListProviderDayAvailability', () => {
     expect(availability).toEqual(
       expect.arrayContaining([
         { hour: 8, available: false },
+        { hour: 9, available: false },
         { hour: 10, available: false },
-        { hour: 11, available: true },
-        { hour: 17, available: true },
+        { hour: 13, available: true },
+        { hour: 14, available: false },
+        { hour: 15, available: false },
+        { hour: 16, available: true },
       ]),
     );
   });
