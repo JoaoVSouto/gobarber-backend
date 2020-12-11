0abce233491e30ddda297bc6777f4c3ff8a86f03
JoaoVSouto <joaovitorss99@hotmail.com>
2020-12-10 23:19:55 -0300

    Creates functionality of listing appointments from a single provider

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -3,19 +3,16 @@ import { Router } from 'express';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController';
+import ProviderAppointmentsController from '@modules/appointments/infra/http/controllers/ProviderAppointmentsController';
 
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 const appointmentsRouter = Router();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-// appointmentsRouter.get('/', async (req, res) => {
-//   const appointments = await appointmentsRepository.find();
-
-//   return res.json(appointments);
-// });
-
+appointmentsRouter.get('/me', providerAppointmentsController.index);
 appointmentsRouter.post('/', appointmentsController.create);
 
 export default appointmentsRouter;
